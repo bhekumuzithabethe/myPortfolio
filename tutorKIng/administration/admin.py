43c8ee7a223b6e0f84c7114d503307f8af0aaffb
Bhekumuzi Thabethe <4123449@myuwc.ac.za>
2026-01-10 08:00:57 +0200

    update resume

--- tutorKIng/administration/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from administration.models import PastQuestionPaper,Learner
-
-# Register your models here.
-admin.site.register(PastQuestionPaper)
-admin.site.register(Learner)
--- tutorKIng/staticfiles/js/quiz.js
+++
@@ -1,9 +0,0 @@
-const url = window.location.href
-console.log('quiz.js running')
-$.ajax({
-    type: 'GET',
-    url:`${url}data`,
-    success : function (response) {
-        console.log(response)
-    }
-})
